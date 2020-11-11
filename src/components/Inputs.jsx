bbe46f41e1cd6461829170511fdbde1ba19142c1
Syam Iyer <iyersyam@gmail.com>
2020-11-12 00:50:30 +1100

    Got autocomplete working

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,48 +1,66 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import _ from 'lodash';
+import axios from 'axios';
+
+function getProgramList(query) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            var config = {
+                method: 'post',
+                url: 'http://localhost:3000/autocompletePrograms',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: { query }
+            };
+
+            axios(config)
+                .then(function (response) {
+                    resolve(response.data);
+                })
+                .catch(function (error) {
+                    console.log("AXIOS ERROR GETTING PROGRAM LIST", error);
+                    reject(error);
+                });
+        } catch (ex) {
+            console.log("EXCEPTION GETTING PROGRAM LIST", ex);
+            reject(ex);
+        }
+    });
+}
 
 function Inputs(props) {
-    const [value, setValue] = useState("");
+    const [query, setQuery] = useState('');
+    const [searchQuery, setSearchQuery] = useState({});
+    const [dataList, setDataList] = useState([]);
 
-    const handleInputChange = useCallback(
-        (e) => {
-            const value = e.target.value;
-            setValue(value);
-            debounceHandler(value);
-        },
-        [debounceHandler]
-    );
+    const handleChange = ({ target: { value } }) => {
+        setQuery(value);
 
-    const debounceHandler = useCallback(
-        _.debounce((value) => {
-            console.log(`Debounced Value = ${value}`);
-        }, 1000),
-        []
-    );
+        const search = _.debounce(sendQuery, 300);
 
+        search(value);
+    };
+
+    const sendQuery = async value => {
+        const programs = await getProgramList(value);
+
+        setDataList(programs);
+    };
 
     return (
         <div className="jumbotron">
+            {/* {dataList && dataList.length && dataList.map((program, i) => {
+                return <div key={i + program.item.Item.code.S}> {program.item.Item.code.S}: {program.item.Item.title.S} ({program.item.Item.implementation_year.S})</div>
+            })} */}
             {/* Programs */}
             <div className="form-group">
                 <label>Program</label>
-                <input onChange={handleInputChange} className="form-control" list="browsers" name="browser"/>
-                <datalist id="browsers">
-                    <option value="Firefox" />
-                    <option value="Chrome" />
-                    <option value="Opera" />
-                    <option value="Safari" />
-                </datalist>
-            </div>
-
-            {/* Year */}
-            <div className="form-group">
-                <label>Year</label>
-                <input className="form-control" list="years" name="years"/>
-                <datalist id="years">
-                    <option>2019</option>
-                    <option>2020</option>
-                    <option>2021</option>
+                <input onChange={handleChange} className="form-control" list="programs" name="browser"/>
+                <datalist id="programs">
+                    {dataList && dataList.length && dataList.map((program, i) => {
+                        return <option key={i + program.item.Item.code.S}> {program.item.Item.code.S}: {program.item.Item.title.S} ({program.item.Item.implementation_year.S}) </option>
+                    })}
                 </datalist>
             </div>
 
