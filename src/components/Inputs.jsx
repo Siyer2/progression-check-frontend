54c0ba12c1325ef2358e4012ad214087c90c2ebc
Syam Iyer <iyersyam@gmail.com>
2020-11-13 17:35:59 +1100

    Connected react select with state

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import _ from 'lodash';
 import axios from 'axios';
-import { Form } from 'react-bootstrap';
+import { Form, Col, Button } from 'react-bootstrap';
 import AsyncSelect from 'react-select/async'
 
 function getProgramList(query) {
@@ -63,22 +63,12 @@ function getProgram(code, year) {
 }
 
 function Inputs() {
-    const [query, setQuery] = useState('');
+    const [programInput, setProgramInput] = useState('');
     const [programError, setProgramError] = useState('unset');
     const [dataList, setDataList] = useState([]);
     const [selectedProgram, setSelectedProgram] = useState('');
     const [selectedSpecialisations, setSelectedSpecialisations] = useState({});
     const [specialisationError, setSpecialisationError] = useState({});
-    
-    const handleChange = ({ target: { value } }) => {
-        console.log(value);
-        setProgramError('unset');
-        setQuery(value);
-
-        const search = _.debounce(sendQuery, 300);
-
-        search(value);
-    };
 
     const handleSpecialisationChange = ({ target: { value, name } }) => {
         setSelectedSpecialisations(prevState => {
@@ -89,17 +79,10 @@ function Inputs() {
         });
     };
 
-    const sendQuery = async value => {
-        const programs = await getProgramList(value);
-
-        setDataList(programs);
-    };
-
     async function programAdded() {
         // See if it's a valid program
-        const code = query.split(':')[0];
-        const year = query.substring(query.length - 5, query.length - 1);
-        console.log("datalist", code, year);
+        const code = programInput.code;
+        const year = programInput.year;
         
         var programFromDataList = _.find(dataList, function(program) {
             return program.item.Item.code.S === code && program.item.Item.implementation_year.S === year;
@@ -120,26 +103,6 @@ function Inputs() {
         console.log(programFromDataList);
     }
 
-    async function specialisationAdded() {
-        // Check if it's a valid program
-        Object.keys(selectedSpecialisations).map((spec) => {
-            const isValid = _.find(selectedProgram.item.Item[spec.toLowerCase()].L, function(options) {
-                return options.S === selectedSpecialisations[spec];
-            });
-
-            if (!isValid) {
-                setSpecialisationError(prevState => {
-                    return { ...prevState, [spec]: `Couldn't find ${spec} called ${selectedSpecialisations[spec]}` };
-                });
-            }
-            else {
-                setSpecialisationError(prevState => {
-                    return { ...prevState, [spec]: '' };
-                });
-            }
-        });
-    }
-
     function goClicked() {
         console.log("selectedProgram", selectedProgram);
         console.log("selectedSpec", selectedSpecialisations);
@@ -155,7 +118,7 @@ function Inputs() {
                         <Form.Control as="select" custom onChange={handleSpecialisationChange} value={selectedSpecialisations[specialisationType]} name={specialisationType}>
                             <option> </option>
                             {specialisationList && specialisationList.length && specialisationList.map((spec, i) => {
-                                return <option onClick={(e) => { handleClick(e) }} key={i + spec.S}> {spec.S} </option>
+                                return <option key={i + spec.S}> {spec.S} </option>
                             })}
                         </Form.Control>
                     </Form.Group>
@@ -165,7 +128,6 @@ function Inputs() {
     }
 
     function Specialialisations() {
-        console.log(selectedProgram);
         return (
             <>
                 {selectedProgram && selectedProgram.item.Item.majors && SpecificSpecialisation('Majors', selectedProgram.item.Item.majors.L)}
@@ -179,41 +141,39 @@ function Inputs() {
     const promiseOptions = inputValue => {
         return new Promise(async resolve => {
             const programs = await getProgramList(inputValue);
+            setDataList(programs);
             const formattedPrograms = programs.map((program) => {
                 return {
-                    value: program.item.Item.code.S + '_' + program.item.Item.implementation_year.S, 
+                    year: program.item.Item.implementation_year.S, 
+                    code: program.item.Item.code.S,
                     label: `${program.item.Item.code.S}: ${program.item.Item.title.S} (${program.item.Item.implementation_year.S})`
                 }
             });
-            console.log("fp", formattedPrograms);
             resolve(formattedPrograms);
         });
     }
 
+    const handleProgramInputChange = newValue => {
+        setProgramInput(newValue);
+    }
+
     const isGoDisabled = (programError || programError === 'unset') || (specialisationError['Majors'] || specialisationError['Minors'] || specialisationError['Honours'] || specialisationError['Specialisations']);
-    const programInputClass = `form-control ${programError === 'unset' ? '' : programError ? 'is-invalid' : 'is-valid'}`;
     return (
         <div className="jumbotron">
             {/* Programs */}
-            <div className="form-group">
+            <Form>
                 <label>Program</label>
-                <AsyncSelect cacheOptions defaultOptions loadOptions={promiseOptions} placeholder={'Search for a program...'}/>
-            </div>
-
-            <div className="form-group">
-                <div className="input-group mb-3">
-                    <input onChange={handleChange} className={programInputClass} list="programs" name="browser" />
-                    <div className="input-group-append">
-                        <button onClick={() => {programAdded()}} className="btn btn-secondary">Add</button>
-                    </div>
-                    {programError && <div className="invalid-feedback">{programError}</div>}
-                </div>                
-                <datalist id="programs">
-                    {dataList && dataList.length && dataList.map((program, i) => {
-                        return <option onClick={(e) => {handleClick(e)}} key={i + program.item.Item.code.S}> {program.item.Item.code.S}: {program.item.Item.title.S} ({program.item.Item.implementation_year.S}) </option>
-                    })}
-                </datalist>
-            </div>
+                <Form.Row>
+                    <Col>
+                        <AsyncSelect onChange={handleProgramInputChange} cacheOptions defaultOptions loadOptions={promiseOptions} placeholder={'Search for a program...'}/>
+                    </Col>
+                    <Col xs="auto">
+                        <Button onClick={() => { programAdded() }} className="mb-2">
+                            Add
+                        </Button>
+                    </Col>
+                </Form.Row>
+            </Form>
 
             {/* Specialisations */}
             <Specialialisations />
