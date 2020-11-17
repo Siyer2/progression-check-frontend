0fc93dcd378ea15755b0931b3c96ea573a5c8b5f
Syam Iyer <iyersyam@gmail.com>
2020-11-17 14:34:47 +1100

    Adding a course updates the minimum UOC

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import Outputs from './Outputs';
+import { getRemainingRequirements } from '../helperFunctions';
 
 import fakeRequirements from '../fakeRequirements.json';
 
 function Results(props) {
+    const [remainingRequirements, setRemainingRequirements] = useState(fakeRequirements); // This should be props.requirements.requirements not fakeRequirements
+    
     if (!props.requirements.isGettingRequirements) {
         console.log("FINAL", props.requirements.requirements);
     }
 
-    function addedCourse(course) {
-        console.log("ADDED COURSE", course);
+    async function addedCourse(course) {
+        const newRemainingRequirements = await getRemainingRequirements(course, remainingRequirements);
+
+        setRemainingRequirements(prevState => {
+            return {...prevState, newRemainingRequirements}
+        });
     }
 
     // TESTING 
     return (
         <>
-            <Outputs requirements={fakeRequirements} addedCourse={addedCourse}/>
+            <Outputs requirements={remainingRequirements} addedCourse={addedCourse}/>
         </>
     )
 
     // ACTUAL
     return (
         // <Outputs />
-        !props.requirements.isGettingRequirements && !props.requirements.requirements.code ?  <NoSetProgram /> 
+        !props.requirements.isGettingRequirements && !props.requirements.requirements.code ? <NoSetProgram /> 
         :
         <>
             {props.requirements.isGettingRequirements ? 
@@ -33,7 +40,7 @@ function Results(props) {
                 </div>
                     : 
                     <>
-                        <Outputs requirements={props.requirements.requirements}/>
+                        <Outputs requirements={props.requirements.requirements} addedCourse={addedCourse}/>
                     </>
             }
         </>
--- src/helperFunctions/index.js
+++ src/helperFunctions/index.js
@@ -3,3 +3,43 @@ export function stripHtml(html) {
     tmp.innerHTML = html;
     return tmp.textContent || tmp.innerText || "";
 }
+
+//==== ====//
+export async function getRemainingRequirements(newCourse, program) {
+    console.log("program", program);
+    console.log("new course", newCourse);
+    const newUOC = await adjustUOC(newCourse, program);
+
+    program.minimumUOC = newUOC;
+    return program;
+}
+
+// Fix minimum UOC
+function adjustUOC(newCourse, program) {
+    return new Promise((resolve, reject) => {
+        try {
+            const existingUOC = parseInt(program.minimumUOC);
+            const courseUOC = parseInt(newCourse.Item.credit_points.S);
+            const newUOC = existingUOC - courseUOC;
+
+            resolve(newUOC);
+        } catch (ex) {
+            console.log("EXCEPTION ADJUSTING UOC", ex);
+            reject(ex);
+        }
+    });
+}
+
+/*
+// Fix minimum UOC
+function adjustUOC(newCourse, program) {
+    return new Promise((resolve, reject) => {
+        try {
+            
+        } catch (ex) {
+            console.log("EXCEPTION ADJUSTING UOC", ex);
+            reject(ex);
+        }
+    });
+}
+*/
