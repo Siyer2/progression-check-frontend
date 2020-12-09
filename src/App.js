200962f5aa7695d58077cb5c681c63d9c56e5b23
Syam Iyer <iyersyam@gmail.com>
2020-12-10 10:16:13 +1100

    Added a new feature button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './bootstrap.min.css';
 import './App.css';
+import { OverlayTrigger, Button, Popover } from 'react-bootstrap';
 
 import NavBar from './components/NavBar'; 
 import Main from './components/Main';
@@ -11,11 +12,33 @@ function App() {
     <div className="App">
       <NavBar />
       <Main />
+      <footer style={{textAlign: 'left', paddingLeft: '5px'}}>
+        <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+          <Button variant="success">New Features 🎉</Button>
+        </OverlayTrigger>
+      </footer>
       <footer style={{textAlign: 'right'}}>
         <p>By <a href="https://github.com/Siyer2" target="_blank">Syam</a>&nbsp;&nbsp;</p>
       </footer>
     </div>
   );
 }
+
+const newFeatures = [
+  "Double Majors", 
+  "Delete courses", 
+  "Save courses"
+]
+const popover = (
+  <Popover id="popover-basic">
+    <Popover.Title as="h3">11th December, 2020</Popover.Title>
+    {newFeatures.map((feature, i) => {
+      return (
+      <Popover.Content key={i+feature}>
+        {feature}
+     </Popover.Content>)
+    })}
+  </Popover>
+);
 
 export default App;
